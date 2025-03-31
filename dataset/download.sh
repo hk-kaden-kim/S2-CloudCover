04621e8c54204dae7557652f0bf24bf0c718dfed
hk-kaden-kim <hyeongkyun.kim@uzh.ch>
2025-03-31 11:53:23 +0200

    update

---
+++ dataset/download.sh
@@ -0,0 +1,16 @@
+aws configure list
+
+# Echo
+TIME="`date "+%Y-%m-%d %H:%M:%S"`"
+echo $TIME : Start!
+
+AWS_SOURCE=s3://radiantearth/cloud-cover-detection-challenge/final/public
+aws s3 cp $AWS_SOURCE ./public --recursive --quite --endpoint-url=https://data.source.coop &> ./log_download.txt
+
+# Echo
+TIME="`date "+%Y-%m-%d %H:%M:%S"`"
+echo $TIME : Done !
+
+
+
+# scp -i ~/.ssh/s2-cloudcover.pem ~/.aws/config ubuntu@44.223.27.233:~/.aws
